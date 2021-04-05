4ea67c88f880074725a5de91a4944d8b8d907ded
seungho <xlqmdl123@naver.com>
2021-04-05 11:54:36 +0900

    두번째 버전

--- web1.py
+++ web1.py
@@ -10,4 +10,13 @@ print(soup.prettify())
 #<p>를 모두 검색해! 리스트 형식으로 리턴
 # print(soup.find_all("p"))
 #첫번째 <p>를 검색
-print(soup.find("p"))
+# print(soup.find("p"))
+# print(soup.find_all("p", class_="outer-text"))
+# print(soup.find(id="first"))
+
+#태그를 제거하고 내부 문자열(컨텐츠만)
+#<p>컨텐츠<p>
+for tag in soup.find_all("p"):
+    #앞뒤에 공백문자를 제거
+    print(tag.text.strip())
+
